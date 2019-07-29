c5c8bd4086a16f4e7e0485eb1050b9d12dab1bd9
Yong Soo , Yuk <yozx9079@naver.com>
2019-07-29 18:54:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ Vue.use(Vuetify, {
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
-        warning: '#FFC107',
-        dark: true
+        warning: '#FFC107'
     }
 });
 

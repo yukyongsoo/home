0a32e8b7dd4351d1bc59e1e3d7c3b8b669c00fce
yongsoo <yozx9079@naver.com>
2020-02-15 22:39:44 +0900

    add renewal comment

--- view/logo-component.vue
+++ view/logo-component.vue
@@ -1,6 +1,9 @@
 <template>
   <v-img src="assets/section.jpg" :aspect-ratio="16/10">
     <v-layout column align-center justify-center fill-height>
+       <transition appear name="fade">
+        <h3 class="display-1 text-xs-center" style="animation-duration: 15s">홈페이지 리뉴얼 중!!</h3>
+      </transition>
       <transition appear name="fade">
         <h1 class="display-2 mb-5 text-xs-center" style="animation-duration: 5s">Development has never been easier</h1>
       </transition>
